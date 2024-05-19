7a54e87b9a51220a8c4cfd4a36ce3916321ddbf9
BenteBussePXL <bente.busse@student.pxl.be>
2024-05-19 19:42:51 +0200

    PUSH

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,38 @@
 const pg = require('pg');
-
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors')
+const crypto = require('crypto');
+
 
 const port=3000;
 
 const pool = new pg.Pool({
-    user: 'secadv',
-    host: 'db',
-    database: 'pxldb',
-    password: 'ilovesecurity',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     connectionTimeoutMillis: 5000
 })
 
 console.log("Connecting...:")
 
-app.use(cors());
+const allowedOrigins = ['http://localhost:8081', "http://localhost:5432"];
+const corsOptions = {
+    origin: function (origin, callback) {
+        console.log(origin);
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('No access allowed!'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
@@ -29,18 +43,30 @@ app.use(
 app.get('/authenticate/:username/:password', async (request, response) => {
     const username = request.params.username;
     const password = request.params.password;
+    const hash = crypto.createHash('sha512').update(password).digest('hex');
 
-    const query = `SELECT * FROM users WHERE user_name='${username}' and password='${password}'`;
+    if (!username || !password) {
+        return response.status(400).json({ error: 'Username and password are required' });
+    }
+
+    //const usernameRegex = /^[a-zA-Z0-9-]+$/;
+    //if (!usernameRegex.test(username)) {
+    //return response.status(400).json({ error: 'Invalid username format' });
+    //}
+
+    const sanitizedUsername = username.replace(/[^a-zA-Z0-9-]/g, '');
+
+    const query = `SELECT * FROM users WHERE user_name='${sanitizedUsername}' and password='${hash}'`;
     console.log(query);
     pool.query(query, (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)});
-      
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)});
+
 });
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
+    console.log(`App running on port ${port}.`)
 })
 
